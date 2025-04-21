07af4c0633e8f65c43248a9ac443fe2ebf3547d8
GHITU <aghitu@galadriel.robot.georgiatech-metz.fr>
2025-04-21 13:39:05 +0200

    rcnn no attention

---
+++ train.py
@@ -0,0 +1,120 @@
+# Parts inspired from train pipeline from:
+# https://github.com/LaurentVeyssier/Image-Captioning-Project-with-full-Encoder-Decoder-model/tree/master
+# Credit: Laurent Veyssier
+
+from data_loader import *
+from net import *
+import nltk
+
+
+def train_rcnn_no_attention():
+
+    nltk.download("punkt_tab")
+    batch_size = 512
+    n_epochs = 10
+    max_boxes = 36
+    lr = 0.001
+
+    embed_dim = 128
+    hidden_dim = 256
+    device = "cuda"
+
+    train_loader = get_loader_features(
+        batch_size=batch_size,
+        vocab_threshold=5,
+        max_boxes=max_boxes,
+        vocab_file="vocab.pkl",
+    )
+
+    vocab = train_loader.dataset.vocab
+    vocab_length = len(vocab)
+
+    # Sanity check
+    print(
+        "Index of <PAD>  :",
+        vocab("<PAD>") if "<PAD>" in vocab.word2idx else "Not defined",
+    )
+    print("Index of <START>:", vocab("<start>"))
+    print("Index of <END>  :", vocab("<end>"))
+    print("Index of <UNK>  :", vocab("<unk>"))
+
+    decoder = DecoderRNN(2048, embed_dim, hidden_dim, vocab_length)
+
+    decoder.to(device)
+
+    criterion = nn.CrossEntropyLoss(ignore_index=0)
+    criterion = criterion.to(device)
+
+    params = list(decoder.parameters())
+    optimizer = torch.optim.Adam(
+        params, lr=lr, betas=(0.9, 0.999), eps=1e-08
+    )  # "Show & Tell" paper uses SGD with no momentum
+
+    loss_history = []
+
+    for epoch in range(n_epochs):
+
+        current_loss = 0.0
+
+        for batch, (features, captions) in enumerate(train_loader):
+            decoder.train()
+
+            features, captions = features.to(device), captions.to(device)
+
+            optimizer.zero_grad()
+
+            # Features are batches x max_boxes x feature dim
+
+            # Mean non zero boxes
+            mask = (
+                features.abs().sum(dim=2) > 0
+            )  # Sum across feature dim, is dim batches x max_boxes
+            box_counts = mask.sum(dim=1)
+            box_counts[box_counts == 0] = 1  # In rare case where we have 0 boxes
+
+            features = features.sum(dim=1) / box_counts.unsqueeze(1)
+
+            # Forward pass
+            outputs = decoder(features, captions)
+
+            # Flatten for criterion
+            outputs = outputs.reshape(-1, vocab_length)
+            captions = captions.reshape(-1)
+
+            loss = criterion(outputs, captions)
+
+            loss.backward()
+
+            optimizer.step()
+
+            if batch % 100 == 0:
+                print(
+                    f"Epoch [{epoch}/{n_epochs}], batch [{batch}/{len(train_loader)}], Loss: {loss.item():.4f}"
+                )
+
+            current_loss += loss.item()
+
+    loss_history.append(current_loss / len(train_loader))
+
+    torch.save(
+        {
+            "decoder_state_dict": decoder.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "learning_rate": lr,
+            "epochs": n_epochs,
+            "batch_size": batch_size,
+            "max_boxes": max_boxes,
+            "loss_history": loss_history,
+            "embed_dim": embed_dim,
+            "hidden_dim": hidden_dim,
+        },
+        f"caption_model_rcnn_lr_{lr}_epochs_{n_epochs}_batch_size_{batch_size}_max_boxes_{max_boxes}_embed_dim_{embed_dim}_hidden_dim_{hidden_dim}.pth",
+    )
+
+
+def main():
+    train_rcnn_no_attention()
+
+
+if __name__ == "__main__":
+    main()
